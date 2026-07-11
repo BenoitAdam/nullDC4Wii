59fd43ad0e105d6c4eafcf62e81ef02d6598473f
BenoitAdam <adam050986@yahoo.fr>
2026-07-11 15:24:07 +0200

    version change (0.47)

--- wii/main.cpp
+++ wii/main.cpp
@@ -661,7 +661,7 @@ bool displayOptionsMenu()
     printf("\033[2J\033[H");
 
     // --- Row 0: Launch ---
-    printf("%s LAUNCH GAME      (A: Launch | B: Back | 1: More Info | 2: Page %d/%d) a0.46\n",
+    printf("%s LAUNCH GAME      (A: Launch | B: Back | 1: More Info | 2: Page %d/%d) a0.47\n",
            (selectedRow == OPT_LAUNCH) ? ">" : " ", optionsPage + 1, OPT_PAGE_COUNT);
 
     // --- Row 1: Game name (display only) ---
@@ -1313,7 +1313,7 @@ int displayMenuAndSelectFile()
   while (true)
   {
     printf("\033[2J\033[H");
-    printf("\nNullDC4Wii - Alpha 0.46   ");
+    printf("\nNullDC4Wii - Alpha 0.47   ");
     printf("Current directory: %s\n", currentPath);
 
     printf("(-) GRAPHICS: ");
