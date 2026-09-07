52cce566e87d043a0bff7dc55a4cc1d267615f4f
BenoitAdam <adam050986@yahoo.fr>
2026-09-07 16:51:52 +0200

    version change : a0.69

--- wii/main.cpp
+++ wii/main.cpp
@@ -1879,7 +1879,7 @@ static void printOptionsFooter(void)
 {
   int cols, rows;
   CON_GetMetrics(&cols, &rows);
-  printf("\033[%d;1H1-Y: Previous | 2+X: Next | alpha 0.68", rows);
+  printf("\033[%d;1H1-Y: Previous | 2+X: Next | alpha 0.69", rows);
 }
 
 bool displayOptionsMenu()
@@ -3312,7 +3312,7 @@ int displayMenuAndSelectFile()
   while (true)
   {
     printf("\033[2J\033[H");
-    printf("\nNullDC4Wii - alpha 0.68   ");
+    printf("\nNullDC4Wii - alpha 0.69   ");
     printf("Current directory: %s\n", currentPath);
 
     printf("Select a game file: (GDI/CDI/BIN/CUE works)\n\n");
